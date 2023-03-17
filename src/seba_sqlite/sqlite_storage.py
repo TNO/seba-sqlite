35cc4bcf31349ed92b5875aa26ac2ae27c0919b3
Peter Verveer <pieter.verveer@tno.nl>
2023-03-17 16:32:11 +0100

    Update sqlite_storage code

--- src/seba_sqlite/sqlite_storage.py
+++ src/seba_sqlite/sqlite_storage.py
@@ -1,9 +1,11 @@
 import copy
 import logging
+import os
 import sqlite3
 import time
 from collections import namedtuple
 from itertools import count
+from pathlib import Path
 
 import numpy
 
@@ -35,9 +37,9 @@ class SqliteStorage:
     # snapshot code, since it is meant for backwards compatibility, and should
     # not be extended with new functionality.
 
-    def __init__(self, output_dir):
+    def __init__(self, workflow, output_dir):
         # Optimization configuration.
-        self._workflow = None
+        self._workflow = workflow
 
         # Internal variables.
         self._output_dir = output_dir
@@ -45,6 +47,7 @@ class SqliteStorage:
         self._control_ensemble_id = 0
         self._gradient_ensemble_id = 0
         self._simulator_results = None
+        self._merit_file = Path(output_dir) / "dakota" / "OPT_DEFAULT.out"
 
     @property
     def file(self):
@@ -54,6 +57,7 @@ class SqliteStorage:
         self._database.add_experiment(
             name="optimization_experiment", start_time_stamp=time.time()
         )
+
         # Add configuration values.
         for control_name, initial_value, lower_bound, upper_bound in zip(
             _convert_names(self._workflow.config.variables.names),
@@ -64,10 +68,11 @@ class SqliteStorage:
             self._database.add_control_definition(
                 control_name, initial_value, lower_bound, upper_bound
             )
+
         for name, weight, scale in zip(
             self._workflow.config.objective_functions.names,
             self._workflow.config.objective_functions.weights,
-            self._workflow.objective_scales,
+            self._workflow.config.objective_functions.scales,
         ):
             self._database.add_function(
                 name=name,
@@ -75,10 +80,11 @@ class SqliteStorage:
                 weight=weight,
                 normalization=1.0 / scale,
             )
+
         if self._workflow.config.nonlinear_constraints is not None:
             for name, scale, rhs_value, constraint_type in zip(
                 self._workflow.config.nonlinear_constraints.names,
-                self._workflow.constraint_scales,
+                self._workflow.config.nonlinear_constraints.scales,
                 self._workflow.config.nonlinear_constraints.rhs_values,
                 self._workflow.config.nonlinear_constraints.types,
             ):
@@ -89,6 +95,7 @@ class SqliteStorage:
                     rhs_value=rhs_value,
                     constraint_type=ConstraintType(constraint_type).name.lower(),
                 )
+
         for name, weight in zip(
             self._workflow.config.realizations.names,
             self._workflow.config.realizations.weights,
@@ -209,80 +216,69 @@ class SqliteStorage:
                 constraint_results[idx] *= -1.0
         return constraint_results
 
-    def _handle_finished_batch_event(self, event):
+    def _handle_finished_batch_event(self, event, result):
         logger.debug("Storing batch results in the sqlite database")
-        last_result = self._workflow.result
 
-        last_batch = last_result.evaluations
-        batch = last_result.batch
-
-        self._add_batch(last_batch.variables, last_batch.perturbed_variables)
-        self._add_simulations(batch, last_result)
+        self._add_batch(
+            result.evaluations.variables, result.evaluations.perturbed_variables
+        )
+        self._add_simulations(result.batch, result)
 
         # Convert back the simulation results to the legacy format:
-        objective_results = last_batch.objectives
-        if last_batch.perturbed_objectives is not None:
-            perturbed_objectives = last_batch.perturbed_objectives.reshape(
-                last_batch.perturbed_objectives.shape[0], -1
+        objective_results = result.evaluations.objectives
+        if result.evaluations.perturbed_objectives is not None:
+            perturbed_objectives = result.evaluations.perturbed_objectives.reshape(
+                result.evaluations.perturbed_objectives.shape[0], -1
             )
             if objective_results is None:
                 objective_results = perturbed_objectives
             else:
                 objective_results = numpy.hstack(
-                    (last_batch.objectives, perturbed_objectives),
+                    (result.evaluations.objectives, perturbed_objectives),
                 )
 
-        constraint_results = last_batch.constraints
+        constraint_results = result.evaluations.constraints
         if self._workflow.config.nonlinear_constraints is not None:
-            if last_batch.perturbed_constraints is not None:
-                perturbed_constraints = last_batch.perturbed_constraints.reshape(
-                    last_batch.perturbed_constraints.shape[0], -1
+            if result.evaluations.perturbed_constraints is not None:
+                perturbed_constraints = (
+                    result.evaluations.perturbed_constraints.reshape(
+                        result.evaluations.perturbed_constraints.shape[0], -1
+                    )
                 )
                 if constraint_results is None:
                     constraint_results = perturbed_constraints
                 else:
                     constraint_results = numpy.hstack(
-                        (last_batch.constraints, perturbed_constraints),
+                        (result.evaluations.constraints, perturbed_constraints),
                     )
             # The legacy code converts all constraints to the form f(x) >=0:
             constraint_results = self._convert_constraints(constraint_results)
 
-        self._add_simulator_results(batch, objective_results, constraint_results)
+        self._add_simulator_results(result.batch, objective_results, constraint_results)
         if self._workflow.config.nonlinear_constraints:
-            self._add_constraint_values(batch, constraint_results)
-        if last_result.functions is not None:
-            self._add_total_objective(last_result.functions.weighted_objective)
-        if last_result.gradients is not None:
-            self._add_gradients(last_result.gradients.objectives)
+            self._add_constraint_values(result.batch, constraint_results)
+        if result.functions is not None:
+            self._add_total_objective(result.functions.weighted_objective)
+        if result.gradients is not None:
+            self._add_gradients(result.gradients.objectives)
 
-        merit_values = (
-            self._workflow.optimizer.get_merit_values()
-            if hasattr(self._workflow.optimizer, "get_merit_values")
-            else []
-        )
-        self._database.update_calculation_result(merit_values)
+        # Merit values are dakota specific, load them if the output file exists:
+        self._database.update_calculation_result(_get_merit_values(self._merit_file))
 
         backup_data(self._database.location)
 
     def _handle_finished_event(self, event):
         logger.debug("Storing final results in the sqlite database")
-        merit_values = (
-            self._workflow.optimizer.get_merit_values()
-            if hasattr(self._workflow.optimizer, "get_merit_values")
-            else []
-        )
-        self._database.update_calculation_result(merit_values)
+        self._database.update_calculation_result(_get_merit_values(self._merit_file))
         self._database.set_experiment_ended(time.time())
 
-    def handle_storage_event(self, event, workflow):
+    def handle_storage_event(self, event, result):
         if event == OptimizationEvent.START_OPTIMIZATION:
-            if self._workflow is None:
-                self._workflow = workflow
-                self._initialize()
+            self._initialize()
         elif event == OptimizationEvent.FINISHED_OPTIMIZATION:
             self._handle_finished_event(event)
         elif event == OptimizationEvent.FINISHED_BATCH:
-            self._handle_finished_batch_event(event)
+            self._handle_finished_batch_event(event, result)
 
     def get_optimal_result(self):
         snapshot = SebaSnapshot(self._output_dir)
@@ -307,10 +303,59 @@ class SqliteStorage:
         )
 
 
-def backup_data(database_location: str) -> None:
+def backup_data(database_location) -> None:
     src = sqlite3.connect(database_location)
     dst = sqlite3.connect(database_location + ".backup")
     with dst:
         src.backup(dst)
     src.close()
     dst.close()
+
+
+def _get_merit_fn_lines(merit_path):
+    if os.path.isfile(merit_path):
+        with open(merit_path, "r", errors="replace", encoding="utf-8") as reader:
+            lines = reader.readlines()
+        start_line_idx = -1
+        for inx, line in enumerate(lines):
+            if "Merit" in line and "feval" in line:
+                start_line_idx = inx + 1
+            if start_line_idx > -1 and line.startswith("="):
+                return lines[start_line_idx:inx]
+        if start_line_idx > -1:
+            return lines[start_line_idx:]
+    return []
+
+
+def _parse_merit_line(merit_values_string):
+    values = []
+    for merit_elem in merit_values_string.split():
+        try:
+            values.append(float(merit_elem))
+        except ValueError:
+            for elem in merit_elem.split("0x")[1:]:
+                values.append(float.fromhex("0x" + elem))
+    if len(values) == 8:
+        # Dakota starts counting at one, correct to be zero-based.
+        return int(values[5]) - 1, values[4]
+    return None
+
+
+def _get_merit_values(merit_file):
+    # Read the file containing merit information.
+    # The file should contain the following table header
+    # Iter    F(x)    mu    alpha    Merit    feval    btracks    Penalty
+    # :return: merit values indexed by the function evaluation number
+    # example:
+    #     0: merit_value_0
+    #     1: merit_value_1
+    #     2  merit_value_2
+    #     ...
+    # ]
+    merit_values = []
+    if merit_file.exists():
+        for line in _get_merit_fn_lines(merit_file):
+            value = _parse_merit_line(line)
+            if value is not None:
+                merit_values.append({"iter": value[0], "value": value[1]})
+    return merit_values
